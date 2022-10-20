45fa65b9367870254187369b3dee0a9143c33804
Fts44 <calmajoseph7@gmail.com>
2022-10-20 22:15:57 +0800

    admin components

---
+++ resources/views/Components/Admin/Sidebar.blade.php
@@ -0,0 +1,43 @@
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="" id="sidebar_message">
+                <i class="bi bi-columns-gap"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" id="sidebar_accounts"  data-bs-target="#accounts-nav" data-bs-toggle="collapse" >
+                <i class="bi bi-people"></i>
+                <span>Accounts</span>
+                <i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="accounts-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i><span>Employees</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i><span>Patients</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="bi bi-circle"></i><span>Requests</span>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+    </li>
+
+</ul>
+
+</aside>
