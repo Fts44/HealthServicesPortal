873543f7ea7f9a2b93ecbd8630cff558bcd1863b
Fts44 <calmajoseph7@gmail.com>
2022-10-24 19:23:53 +0800

    Update page-nav

--- resources/views/Admin/Configuration/Inventory/Equipment/Type.blade.php
+++ resources/views/Admin/Configuration/Inventory/Equipment/Type.blade.php
@@ -12,12 +12,12 @@
             <nav class="btn-group">   
                 <a class="btn btn-sm btn-outline-primary">Name</a>
                 <a class="btn btn-sm btn-outline-primary">Brand</a>
-                <a href="{{ route('AdminConfigurationInventoryEquipmentType') }}" class="btn btn-sm btn-outline-primary">Type</a>           
-                <a class="btn btn-sm btn-outline-primary active">Place</a>
+                <a href="{{ route('AdminConfigurationInventoryEquipmentType') }}" class="btn btn-sm btn-outline-primary active">Type</a>           
+                <a href="{{ route('AdminConfigurationInventoryEquipmentPlace') }}" class="btn btn-sm btn-outline-primary">Place</a>
             </nav>
         </div>
     </div>
-
+   
     @if(session()->has('status'))
         @php $status = (object)session('status') @endphp
         <div class="alert {{ ($status->status==200) ? 'alert-success' : 'alert-danger' }} alert-success alert-dismissible fade show" role="alert">
@@ -33,34 +33,34 @@
 
             <div class="card-body px-4">
 
-                <h5 class="card-title">Equipment Place</h5>
+                <h5 class="card-title">Equipment Type</h5>
                 <a href="#" id="add" class="btn btn-secondary btn-sm" style="float: right; margin-top: -2.5rem;">
                     <i class="bi bi-plus-lg"></i>          
                 </a>
-                <table id="table_equipment_place" class="table table-bordered" style="width: 100%;">
+                <table id="datatable" class="table table-bordered" style="width: 100%;">
                     <thead class="table-light">
                         <th scope="col">ID</th>
-                        <th scope="col">Place</th>
+                        <th scope="col">Type</th>
                         <th scope="col">Status</th>
                         <th scope="col">Action</th>
                     </thead>
                     <tbody>
-                    @foreach($ie_places as $item)
+                    @foreach($ie_types as $item)
                         <tr>
-                            <td>{{ $item->iep_id }}</td>
-                            <td>{{ $item->iep_place }}</td>
+                            <td>{{ $item->iet_id }}</td>
+                            <td>{{ $item->iet_type }}</td>
                             <td>
-                                <span class="badge {{ ($item->iep_status) ? 'bg-success' : 'bg-secondary' }}">{{ ($item->iep_status) ? 'Enabled' : 'Disabled' }}</span>
+                                <span class="badge {{ ($item->iet_status) ? 'bg-success' : 'bg-secondary' }}">{{ ($item->iet_status) ? 'Enabled' : 'Disabled' }}</span>
                             </td>
-                            @if($item->iep_id=='1' && $item->iep_place=='none')
+                            @if($item->iet_id=='1' && $item->iet_type=='none')
                                 <td>
                                     <button class="btn btn-primary btn-sm" disabled><i class="bi bi-pencil"></i></button>
                                     <button class="btn btn-danger btn-sm" disabled><i class="bi bi-eraser"></i></button>
                                 </td>
                             @else
                                 <td>
-                                    <a class="btn btn-primary btn-sm" onclick="update('{{ $item->iep_id }}','{{ $item->iep_place }}','{{ $item->iep_status }}')"><i class="bi bi-pencil"></i></a>
-                                    <button class="btn btn-danger btn-sm" {{ ($item->iei_id!=null) ? 'disabled' : '' }} onclick="return delete_confirmation('{{ $item->iep_place }}','{{ route('AdminConfigurationInventoryEquipmentPlaceDelete', ['id' => ($item->iei_id) ? 'id' : $item->iep_id]) }}');"><i class="bi bi-eraser"></i></button>
+                                    <a class="btn btn-primary btn-sm" onclick="update('{{ $item->iet_id }}','{{ $item->iet_type }}','{{ $item->iet_status }}')"><i class="bi bi-pencil"></i></a>
+                                    <button class="btn btn-danger btn-sm" {{ ($item->ieid_id!=null) ? 'disabled' : '' }} onclick="return delete_confirmation('{{ $item->iet_type }}','{{ route('AdminConfigurationInventoryEquipmentTypeDelete', ['id' => ($item->ieid_id!=null) ? 'id' : $item->iet_id ]) }}');"><i class="bi bi-eraser"></i></button>
                                 </td>
                             @endif
                         </tr>
@@ -80,7 +80,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modal_title">Add Equipment Place</h5>
+                    <h5 class="modal-title" id="modal_title">Add Equipment Type</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form action="" method="POST" id="form" enctype="multipart/form-data">
@@ -89,10 +89,10 @@
 
                     <div class="modal-body mb-4">
                         <label class="form-control border-0 p-0">
-                            Place:
-                            <input class="form-control" type="text" name="place" id="place" value="{{ old('place') }}">
-                            <span class="text-danger" id="error_place">
-                                @error('place')
+                            Type:
+                            <input class="form-control" type="text" name="type" id="type" value="{{ old('type') }}">
+                            <span class="text-danger" id="error_type">
+                                @error('type')
                                     {{ $message }}
                                 @enderror
                             </span>
@@ -133,29 +133,26 @@
     <script src="{{ asset('js/datatable.js') }}"></script>
 
     <script>
-
         function clear(){
             $("#status").prop("selectedIndex", 0);
-            $('#place').val('');
-            $('#error_place, #error_status').html('');
+            $('#type').val('');
+            $('#error_type, #error_status').html('');
         }
-
-        function update(item_id, item_place, item_status){
+        function update(item_id, item_name, item_status){
             clear();
-            var url = "{{ route('AdminConfigurationInventoryEquipmentPlaceUpdate', ['id'=>'id']) }}";
+            var url = "{{ route('AdminConfigurationInventoryEquipmentTypeUpdate', ['id'=>'id']) }}";
             $('#form').attr('action', url.replace('id', item_id));
-            $('#place').val(item_place);
+            $('#type').val(item_name);
             $('#status').val(item_status);
-            $('#modal_title').html('Update Equipment Place');
+            $('#modal_title').html('Update Equipment Type');
             $('#submit_button').html('Update');
             $('#modal').modal('show'); 
         }
-
-        function delete_confirmation(place, href){
+        function delete_confirmation(type, href){
             event.preventDefault();
             swal({
                 title: "Are you sure?",
-                text: "Your about to delete "+place+"!",
+                text: "Your about to delete "+type+"!",
                 icon: "warning",
                 buttons: ["Cancel", "Yes"],
                 dangerMode: true,
@@ -166,13 +163,11 @@
                 }
             });   
         }
-
         $(document).ready(function(){
-
-            datatable_class('#table_equipment_place');
+            datatable_class('#datatable');
             $('#hamburgerMenu').click(function(){
                 setTimeout(function() { 
-                    redraw_datatable_class('#table_equipment_place');
+                    redraw_datatable_class('#datatable');
                 }, 300);
             });
             $('.alert').delay(5000).fadeOut('slow');
@@ -182,26 +177,24 @@
                         $status = (object)session('status');                      
                     @endphp
                     @if($status->action=='Add')
-                        $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentPlaceInsert') }}");
-                        $('#modal_title').html('Add Equipment Place');
+                        $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentTypeInsert') }}");
+                        $('#modal_title').html('Add Equipment Type');
                         $('#submit_button').html('Add');
                     @elseif($status->action=='Update')
-                        $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentPlaceUpdate', ['id' => $status->iep_id]) }}");
-                        $('#modal_title').html('Update Equipment Place');
+                        $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentTypeUpdate', ['id' => $status->iet_id]) }}");
+                        $('#modal_title').html('Update Equipment Type');
                         $('#submit_button').html('Update');
                     @endif
                 @endif                
                 $('#modal').modal('show'); 
             @endif 
-
             $('#add').click(function(){
                 clear();
-                $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentPlaceInsert') }}");
-                $('#modal_title').html('Add Equipment Place');
+                $('#form').attr('action', "{{ route('AdminConfigurationInventoryEquipmentTypeInsert') }}");
+                $('#modal_title').html('Add Equipment Type');
                 $('#submit_button').html('Add');
                 $('#modal').modal('show'); 
             });
-
         });
     </script>
 @endpush
